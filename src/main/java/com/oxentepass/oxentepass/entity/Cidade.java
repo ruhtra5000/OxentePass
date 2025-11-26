850ce94602f30676f267ba7cdbae896a57520169
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-11-25 22:59:50 -0300

    feat: adição de métodos em Cidade

--- src/main/java/com/oxentepass/oxentepass/entity/Cidade.java
+++ src/main/java/com/oxentepass/oxentepass/entity/Cidade.java
@@ -2,6 +2,8 @@ package com.oxentepass.oxentepass.entity;
 
 import java.util.List;
 
+import com.oxentepass.oxentepass.exceptions.TagInvalidaException;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -23,4 +25,29 @@ public class Cidade {
 
     @OneToMany
     private List<Evento> eventos;
+
+    //Métodos
+    public void addTag(Tag tag) {
+        if (this.tags.contains(tag))
+            throw new TagInvalidaException("A tag informada já consta na cidade " + this.nome + ".");
+
+        this.tags.add(tag);
+    }
+
+    public void removerTag(Tag tag) {
+        if (!this.tags.remove(tag)) 
+            throw new TagInvalidaException("A tag informada não consta na cidade " + this.nome + ".");
+    }
+
+    public void addEvento(Evento evento) {
+        if (this.eventos.contains(evento))
+            throw new TagInvalidaException("O evento informado já se encontra vinculado a cidade " + this.nome + ".");
+
+        this.eventos.add(evento);
+    }
+
+    public void removerEvento(Evento evento) {
+        if (!this.eventos.remove(evento))
+            throw new TagInvalidaException("O evento informado não se encontra vinculado a cidade " + this.nome + ".");
+    }
 }
---
+++ src/main/java/com/oxentepass/oxentepass/exceptions/EventoInvalidoException.java
@@ -0,0 +1,9 @@
+package com.oxentepass.oxentepass.exceptions;
+
+public class EventoInvalidoException extends RuntimeException {
+    
+    public EventoInvalidoException (String mensagem) {
+        super(mensagem);
+    }
+
+}
