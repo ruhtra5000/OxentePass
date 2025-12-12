4919bdcc0bb20c798151ef7e868a3ea59a3cd111
PacssauroRex <victorcauaorg@gmail.com>
2025-12-12 14:04:54 -0300

    Alteração do método findByEventoId

--- src/main/java/com/oxentepass/oxentepass/repository/EventoRepository.java
+++ src/main/java/com/oxentepass/oxentepass/repository/EventoRepository.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
