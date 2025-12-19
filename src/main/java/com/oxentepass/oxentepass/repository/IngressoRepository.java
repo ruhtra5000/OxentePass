1143d737a14032ff691322f91b8bd3ecbe8d851c
PacssauroRex <victorcauaorg@gmail.com>
2025-12-19 17:30:27 -0300

    feature (#26) / feature (#29) - implementação das bindings para os repositórios de venda e ingressos

--- src/main/java/com/oxentepass/oxentepass/repository/IngressoRepository.java
+++ src/main/java/com/oxentepass/oxentepass/repository/IngressoRepository.java
@@ -6,18 +6,38 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.oxentepass.oxentepass.entity.Ingresso;
+import com.oxentepass.oxentepass.entity.QIngresso;
+import com.querydsl.core.types.dsl.NumberPath;
+import com.querydsl.core.types.dsl.StringPath;
 
 @Repository
-public interface IngressoRepository extends JpaRepository<Ingresso, Long> { 
+public interface IngressoRepository extends JpaRepository<Ingresso, Long>, QuerydslPredicateExecutor<Ingresso>, QuerydslBinderCustomizer<QIngresso> { 
 
     @Query("select i from Evento e join e.ingressos i where e.id = :idEvento")
     Page<Ingresso> findByEventoId(@Param("idEvento") Long idEvento, Pageable pageable);
 
     public Optional<Ingresso> findByTipo(String tipo);	
-    
-    
+
+    @Override
+    default void customize(QuerydslBindings bindings, QIngresso root) {
+
+        bindings.bind(root.id).first((NumberPath<Long> path, Long value) -> path.eq(value));
+
+        bindings.bind(root.tipo).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+
+        bindings.bind(root.valorBase).first((path, value) -> path.eq(value));
+
+        bindings.bind(root.quantidadeDisponivel).first((path, value) -> path.goe(value));
+
+        bindings.bind(root.temMeiaEntrada).first((path, value) -> path.eq(value));
+        
+    }
+      
 }
--- src/main/java/com/oxentepass/oxentepass/repository/VendaRepository.java
+++ src/main/java/com/oxentepass/oxentepass/repository/VendaRepository.java
@@ -3,13 +3,32 @@ package com.oxentepass.oxentepass.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
+import com.oxentepass.oxentepass.entity.QVenda;
 import com.oxentepass.oxentepass.entity.Venda;
+import com.querydsl.core.types.dsl.NumberPath;
 
 @Repository
-public interface VendaRepository extends JpaRepository<Venda, Long> {
+public interface VendaRepository extends JpaRepository<Venda, Long>, QuerydslPredicateExecutor<Venda>, QuerydslBinderCustomizer<QVenda> {
 
-    public Page<Venda> findByUsuarioId(Long idUsuario, Pageable pageable);
+    Page<Venda> findByUsuarioId(Long idUsuario, Pageable pageable);
 
+    @Override
+    default void customize(QuerydslBindings bindings, QVenda root) {
+
+        bindings.bind(root.id).first((path, value) -> path.eq(value));
+
+        bindings.bind(root.usuario.id).first((NumberPath<Long> path, Long value) -> path.eq(value));
+
+        bindings.bind(root.status).first((path, value) -> path.eq(value));
+
+        bindings.bind(root.valorTotal).first((path, value) -> path.goe(value));
+
+        bindings.bind(root.dataHoraVenda).first((path, value) -> path.after(value));
+        
+    }
 }
