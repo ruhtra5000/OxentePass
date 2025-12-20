3795034767207bda80a352ef5e23102647c9c31f
PacssauroRex <victorcauaorg@gmail.com>
2025-12-19 23:25:54 -0300

    Utilização do Querydsl para listar com filtro

--- src/main/java/com/oxentepass/oxentepass/controller/IngressoController.java
+++ src/main/java/com/oxentepass/oxentepass/controller/IngressoController.java
@@ -2,18 +2,13 @@ package com.oxentepass.oxentepass.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.oxentepass.oxentepass.controller.request.IngressoRequest;
 import com.oxentepass.oxentepass.service.IngressoService;
-
-import jakarta.validation.Valid;
+import com.querydsl.core.types.Predicate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.oxentepass.oxentepass.entity.Ingresso;
 import org.springframework.data.domain.Page;
@@ -28,29 +23,11 @@ public class IngressoController {
     private IngressoService ingressoService;
     
     //Operações Básicas
-    @PostMapping("/cadastro")
-    public ResponseEntity<String> cadastrarIngresso (@RequestBody @Valid IngressoRequest dto) {
-        ingressoService.cadastrarIngresso(dto.paraEntidade());
-
-        return new ResponseEntity<String>(
-            "Ingresso criado com sucesso!", 
-            HttpStatus.CREATED
-        );
-    }
-
-    @DeleteMapping("/deletar/{idIngresso}")
-    public ResponseEntity<String> deletarIngresso (@PathVariable Long idIngresso) {
-        ingressoService.deletarIngresso(idIngresso);
-        return new ResponseEntity<String>(
-            "Ingresso deletado com sucesso!", 
-            HttpStatus.OK
-        );
-    }
 
     @GetMapping("/listar")
-    public ResponseEntity<Page<Ingresso>> listarTodosIngressos (Pageable pageable) {
+    public ResponseEntity<Page<Ingresso>> listarTodosIngressos (Predicate predicate, Pageable pageable) {
         return new ResponseEntity<Page<Ingresso>>(
-            ingressoService.listarTodosIngressos(pageable), 
+            ingressoService.listarTodosIngressos(predicate, pageable), 
             HttpStatus.OK
         );
     }
@@ -72,9 +49,9 @@ public class IngressoController {
     }
 
     @GetMapping("/disponivel/{idEvento}")
-    public ResponseEntity<Page<Ingresso>> quantidadeIngressosDisponiveis (@PathVariable Long idEvento, Pageable pageable) {
+    public ResponseEntity<Page<Ingresso>> quantidadeIngressosDisponiveis (@PathVariable Long idEvento, Predicate predicate, Pageable pageable) {
         return new ResponseEntity<Page<Ingresso>>(
-            ingressoService.ingressosDisponiveis(idEvento, pageable), 
+            ingressoService.ingressosDisponiveis(idEvento, predicate, pageable), 
             HttpStatus.OK
         );
     }
