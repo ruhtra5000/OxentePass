2b89c1aa4f154c4c16cbca499bb6aae2b8d87906
Guilherme Paes <guy.paes176@gmail.com>
2025-12-20 04:42:52 -0300

    feat: limita nota mínima e máxima duma Avaliacao

--- src/main/java/com/oxentepass/oxentepass/controller/request/AvaliacaoRequest.java
+++ src/main/java/com/oxentepass/oxentepass/controller/request/AvaliacaoRequest.java
@@ -2,11 +2,15 @@ package com.oxentepass.oxentepass.controller.request;
 
 import com.oxentepass.oxentepass.entity.Avaliacao;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record AvaliacaoRequest(
         String comentario,
         @NotNull(message = "Nota não pode ser nula")
+        @Min(value = 1, message = "Nota deve ser no mínimo 1")
+        @Max(value = 5, message = "Nota deve ser no máximo 5")
         Integer nota
 ) {
     public Avaliacao paraEntidade() {
