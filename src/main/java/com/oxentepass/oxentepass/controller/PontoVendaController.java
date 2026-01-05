9fe1df77524b602a67f95d371fee48ab33c1900b
Arthur de Sá Tenório <arthursa147@gmail.com>
2026-01-05 20:34:42 -0300

    fix: correçao de anotaçao em UsuarioController e PontoVendaController

--- src/main/java/com/oxentepass/oxentepass/controller/PontoVendaController.java
+++ src/main/java/com/oxentepass/oxentepass/controller/PontoVendaController.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.oxentepass.oxentepass.controller.request.PontoVendaRequest;
 import com.oxentepass.oxentepass.entity.PontoVenda;
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Controller
+@RestController
 @RequestMapping("/pontovenda")
 public class PontoVendaController {
 
--- src/main/java/com/oxentepass/oxentepass/controller/UsuarioController.java
+++ src/main/java/com/oxentepass/oxentepass/controller/UsuarioController.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.oxentepass.oxentepass.controller.request.UsuarioLoginRequest;
 import com.oxentepass.oxentepass.controller.request.UsuarioRequest;
@@ -21,7 +21,7 @@ import com.oxentepass.oxentepass.service.UsuarioService;
 
 import jakarta.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
 
